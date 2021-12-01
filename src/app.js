51f0b99a84b0c7d47f4b442d1169a10f5e00c8ef
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-12-01 21:11:59 +0200

    Node Static Files Server solution added

--- src/app.js
+++ src/app.js
@@ -1,21 +1,30 @@
-'use strict';
+import http from 'http';
+import fs from 'fs';
+import url from 'url';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const PORT = process.env.port || 8080;
 
-module.exports = sum;
+const server = http.createServer((req, res) => {
+  const normalizedURL = new url.URL(req.url, `http://${req.headers.host}`);
+  console.log("normalized url: " + normalizedURL);
+
+  if (!String(normalizedURL).includes('file')) {
+    throw new Error("Pathname should start with `/file/`");
+  }
+
+  const fileName = normalizedURL.pathname.slice(1).replace('file', 'public');
+  console.log("filename: " + fileName);
+
+  fs.readFile(fileName, (err, data) => {
+    if (err) {
+      res.statusCode = 404;
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}...`);
+})
