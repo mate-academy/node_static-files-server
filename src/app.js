f377fae8689069984cd8e420b899d1825c0c1965
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-12-01 21:15:08 +0200

    Unnecessary console.logs removed

--- src/app.js
+++ src/app.js
@@ -6,14 +6,12 @@ const PORT = process.env.port || 8080;
 
 const server = http.createServer((req, res) => {
   const normalizedURL = new url.URL(req.url, `http://${req.headers.host}`);
-  console.log("normalized url: " + normalizedURL);
 
   if (!String(normalizedURL).includes('file')) {
     throw new Error("Pathname should start with `/file/`");
   }
 
   const fileName = normalizedURL.pathname.slice(1).replace('file', 'public');
-  console.log("filename: " + fileName);
 
   fs.readFile(fileName, (err, data) => {
     if (err) {
