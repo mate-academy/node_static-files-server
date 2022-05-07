44965f9679adabd4759a6a3354d4faceec04154a
ArtemTopolskyi <artem.t@mate.academy>
2022-05-07 17:27:26 +0300

    created server

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const requestURL = new URL(req.url, `http://${req.headers.host}`);
+
+  const pathname = requestURL.pathname;
+
+  if (!pathname.startsWith('/file')) {
+    res.statusCode = 404;
+
+    return res.end(`URL must be started with http://${req.headers.host}/file/`);
+  }
+
+  const fileName = pathname.replace('/file', '').slice(1) || 'index.html';
+  const filePath = `public/${fileName}`;
+
+  fs.readFile(filePath, (error, data) => {
+    if (error) {
+      res.statusCode = 404;
+
+      res.write('Page not found');
+    } else {
+      res.write(data);
+    }
+
+    res.end();
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
