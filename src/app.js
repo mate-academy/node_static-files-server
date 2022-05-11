71d57127a6edfc5921afd592b80b15163b64bd25
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-05-11 17:47:12 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,33 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs/promises');
+const http = require('http');
+const url = require('url');
 
-module.exports = sum;
+const server = http.createServer((req, res) => {
+  const { pathname } = new url.URL(req.url, `http://${req.headers.host}`);
+
+  console.log(pathname);
+
+  const splittedPath = pathname.split('/');
+
+  console.log(splittedPath);
+
+  if (splittedPath[1] !== 'file' && !!splittedPath[1]) {
+    res.statusCode = 400;
+    res.end('request must start with \'/file/\'');
+  }
+
+  fs.readFile(`./public/${splittedPath.slice(2).join('/') || 'index.html'}`)
+    .then(data => {
+      res.statusCode = 200;
+      res.end(data);
+    })
+    .catch(() => {
+      res.statusCode = 404;
+      res.end('file is not exist');
+    });
+});
+
+server.listen(3000);
