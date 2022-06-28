723342b43272405bce14a5b74ff13a64dd51154d
Dima Permiakov <permiakov.d.s@gmail.com>
2022-06-28 18:56:44 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -6,21 +6,32 @@ const fs = require('fs');
 const PORT = process.env.port || 8080;
 
 const server = http.createServer((req, res) => {
-  let fileName = req.url.slice(1).replace('file', '') || 'index.html';
+  let fileName = req.url.slice(1);
 
-  if (fileName.includes('/') && fileName.length < 2) {
-    fileName = 'index.html';
+  if (fileName.includes('/') || fileName.length < 1) {
+    fileName = 'file/index.html';
   }
 
-  fs.readFile(`./public/${fileName}`, (error, data) => {
-    if (!error) {
-      res.end(data);
-    }
+  const checkParth = fileName.split('/')[0];
 
-    res.statusCode = 404;
+  // eslint-disable-next-line no-console
+  console.log(fileName);
 
-    res.end('Sample: http://localhost:8080/file/index.html');
-  });
+  if (!checkParth.includes('file')) {
+    res.end('Sample: "html://localhost:8080/file/index.html"');
+  } else {
+    fileName = fileName.split('/').slice(1).join('/') || 'index.html';
+
+    fs.readFile(`./public/${fileName}`, (error, data) => {
+      if (!error) {
+        res.end(data);
+      } else {
+        res.statusCode = 404;
+
+        res.end('Error code 404!!!');
+      }
+    });
+  }
 });
 
 server.listen(PORT, () => {
