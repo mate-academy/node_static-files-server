86474b7f89f16234af27198624e48af759dcab04
Cyril Prue <kir8mir>
2022-10-18 00:26:45 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,33 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-module.exports = sum;
+const PORT = process.env.port || 8080;
+
+const server = http.createServer((req, res) => {
+  const normalizedURL = new url.URL(req.url, `http://${req.headers.host}`);
+  const myURL = normalizedURL.pathname.slice(1) || 'index.html';
+
+  if (myURL.includes('file')) {
+    fs.readFile(`./src/public/${myURL.replace('file/', '')}`, (err, data) => {
+      if (err) {
+        console.error(err);
+        res.statusCode = 404;
+
+        res.end();
+      } else {
+        res.statusCode = 200;
+        res.end(data);
+      }
+    });
+  } else {
+    console.log('Error: write /file/<YOUR_FILE>');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
