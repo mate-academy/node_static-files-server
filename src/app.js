5b11376256de0706fee225074f4dfcd11ddbb0da
Dmytro Semenov <Dmytro.Semenov@tx.local>
2022-10-30 23:08:46 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,45 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
 
-module.exports = sum;
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const CORRECT_FOLDER = 'file';
+const ERROR = {
+  FILE_NOT_EXIST: 'Can not read this file!',
+  WRONG_PATHNAME: `The pathname must start with '/file'`,
+};
+
+const server = http.createServer((req, res) => {
+  const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+  const correctURL = normalizedURL.pathname.slice(1);
+  const arrayOfPathname = correctURL.split('/');
+
+  const folder = arrayOfPathname[0];
+  const secondPath = arrayOfPathname.length >= 2 ? arrayOfPathname[1] : '';
+  const fileName = secondPath.length
+    ? normalizedURL.pathname.replace('/file', '')
+    : 'index.html';
+
+  if (folder === CORRECT_FOLDER) {
+    fs.readFile(`./public/${fileName}`, 'utf8', (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end(ERROR.FILE_NOT_EXIST);
+      } else {
+        res.end(data);
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end(ERROR.WRONG_PATHNAME);
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
