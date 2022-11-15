600e76f9cb21ce91dd4a7aacd50520ed7f3efe6b
Ihor Andrianov <andrianoia@gmail.com>
2022-11-15 16:19:59 +0200

    fixed after review

--- src/app.js
+++ src/app.js
@@ -1,31 +1,34 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const http = require('http')
+const fs = require("fs");
+const http = require("http");
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
-  const normalizedURL = new URL(req.url, `http://localhost:${PORT}`)
-  if (normalizedURL.pathname.slice(1).split('/')[0] !== 'file') {
-    res.writeHead(400, 'Bad Request').end('Pathname should contain /file/...')
-  } else {
-    const fileName = normalizedURL.pathname.slice(1).replace('file', 'public')
-    const correctedFileName = fileName === 'public/' || fileName === 'public'
-      ?  'public/index.html'
-      : fileName
-    fs.readFile(`./${correctedFileName}`, (error, data) => {
-      if (error) {
-        res.writeHead(404, 'Page not found').end('Page not found')
-      }
-      res.end(data)
-    })
+  const normalizedURL = new URL(req.url, `http://localhost:${PORT}`);
+  const path = normalizedURL.pathname.slice(1);
+  if (path.split("/")[0] !== "file") {
+    res.writeHead(400, "Bad Request").end("Pathname should contain /file/...");
+    return;
   }
 
+  const fileName = path.replace("file", "public");
+  const correctedFileName =
+    fileName === "public/" || fileName === "public"
+      ? "public/index.html"
+      : fileName;
 
+  fs.readFile(`./${correctedFileName}`, (error, data) => {
+    if (error) {
+      res.writeHead(404, "Page not found").end("Page not found");
+      return;
+    }
+    res.end(data);
+  });
 });
 
-// Enables the server
 server.listen(PORT, () => {
+  //eslint-disable-next-line
   console.log(`Server is running on http://localhost:${PORT}`);
 });
