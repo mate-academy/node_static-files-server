d328450464c5b86ad9b66b69a1472aa90e0e5312
Vlad Malinin <vladmalinin1999@gmail.com>
2022-11-18 16:20:33 +0200

    Static files server

--- src/app.js
+++ src/app.js
@@ -1,21 +1,41 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
 
-module.exports = sum;
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+  const pathToFile = normalizedURL.pathname.slice(1);
+
+  if (pathToFile.split('/')[0] !== 'file') {
+    res.end('Pathname must contain "/file" at the beginning');
+
+    return;
+  }
+
+  const mainPath = path
+    .join(__dirname, '/public/' + pathToFile
+      .split('/')
+      .slice(1)
+      .join('/')
+    );
+
+  fs.readFile(mainPath, (error, data) => {
+    if (error) {
+      res.statusCode = 404;
+      res.end('Page not found');
+
+      return;
+    }
+
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server is running on: http://localhost:' + PORT);
+});
