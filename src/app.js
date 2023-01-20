a762fc475d18070aae8973dd1f110e8ec69ca879
Vladyslav <vladyslav3177@gmail.com>
2023-01-21 00:14:18 +0200

    static files server

--- src/app.js
+++ src/app.js
@@ -1,21 +1,34 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
+
+  if (!normalizedUrl.pathname.startsWith('/file')) {
+    res.end('The file should start with /file');
+  }
+
+  const fileName = normalizedUrl.pathname
+    .split('/')
+    .slice(2)
+    .join('/')
+    || 'index.html';
+
+  fs.readFile(`./public/${fileName}`, (err, data) => {
+    if (err) {
+      res.statusCode = 404;
+      res.end();
+    }
+
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`The server is listening on port ${PORT}`);
+});
