a30616bc5fcdb46e638ec3aace71359b2e6285dd
Дмитро Ніколенко <dmitronikolenko@MacBook-Air-Dmitro.local>
2023-01-24 19:04:17 +0200

    done!

--- src/app.js
+++ src/app.js
@@ -1,21 +1,40 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/favicon.ico') {
+    return;
+  }
+
+  let { pathname } = new URL(req.url, `http://${req.headers.host}`);
+
+  if (req.url === '/file') {
+    pathname += '/';
+  }
+
+  if (!pathname.startsWith('/file/')) {
+    res.end('not available path, try .../file/<filename>');
+
+    return;
+  }
+
+  res.setHeader('Content-Type', 'text/html; charset="UTF-8');
+
+  const filepath = pathname.replace('/file/', '') || 'index.html';
+
+  fs.readFile(`./public/${filepath}`, (error, data) => {
+    if (error) {
+      res.statuseCode = 404;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(error));
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(PORT, () => {});
