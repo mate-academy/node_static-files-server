69aad988638913dccdfd2f92d939b9f8c190f6aa
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-01-31 20:23:17 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,29 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const fs = require('fs');
+const http = require('http');
 
-module.exports = sum;
+const PORT = process.env.port || 8080;
+
+const server = http.createServer((req, res) => {
+  const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = normalizedURL.pathname;
+
+  if (!pathname.startsWith('/file')) {
+    res.end('Enter correct path name!');
+  }
+
+  const fileName = pathname.split('/').slice(2).join('/') || 'index.html';
+
+  fs.readFile(`./public/${fileName}`, (err, data) => {
+    if (!err) {
+      res.statusCode = 200;
+      res.end(data);
+    }
+
+    res.statusCode = 404;
+    res.end();
+  });
+});
+
+server.listen(PORT);
