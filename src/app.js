0ed4f885133e6fb93b1fabfb89c64ec7ac505c47
ysrckr <dyt.yasarcakir@gmail.com>
2023-02-13 15:21:20 +0300

    done

--- src/app.js
+++ src/app.js
@@ -2,13 +2,34 @@
 'use strict';
 
 const http = require('http');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World');
+  const clearUrl = new URL(req.url, `http://${req.headers.host}`);
+  const { pathname } = clearUrl;
+
+  const pathArr = pathname.split('/').filter((item) => item !== '');
+
+  if (pathArr[0] === 'file') {
+    const path = pathArr.splice(1).join('/') || 'index.html';
+
+    fs.readFile(`./public/${path}`, (err, data) => {
+      if (!err) {
+        res.statusCode = 200;
+        res.end(data);
+      } else {
+        res.statusCode = 404;
+        res.end('404 Not Found');
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Please, use /file/');
+  }
 });
 
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server on http://localhost:${PORT}`);
 });
