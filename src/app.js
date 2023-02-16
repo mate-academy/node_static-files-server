fbceb41d07df9e559cc290e6e8d633acb4762f1a
Roman Bliashuk <romanblyashuk94@gmail.com>
2023-02-16 14:46:42 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,44 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs/promises');
 
-module.exports = sum;
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer(async (req, resp) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  if (!url.pathname.startsWith('/file')) {
+    resp.statusCode = 400;
+    resp.statusMessage = 'Bad request'
+
+    resp.end('pathname should start from "/file/"');
+
+    return
+  }
+
+  let filePath = `src/public/${url.pathname.slice(6) ||  'index.html'}`;
+
+  const isExtensionInFileName = filePath.slice(filePath.lastIndexOf('/')).includes('.');
+  
+  if (!isExtensionInFileName) {
+    filePath += '.html'
+  }
+
+  try {
+    const page = await fs.readFile(filePath, 'utf8');
+
+    resp.statusCode = 200;
+    resp.statusMessage = 'OK';
+    resp.end(page)
+  } catch (error) {
+    resp.statusCode = 404;
+    resp.statusMessage = 'Page not foud'
+    resp.end(`${url.pathname} not found!`)
+  }
+
+})
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
