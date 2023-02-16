e9ac83c81b0db74abb85c2d51be36ed01b594328
ysrckr <dyt.yasarcakir@gmail.com>
2023-02-16 14:58:38 +0300

    after review

--- src/app.js
+++ src/app.js
@@ -10,24 +10,35 @@ const server = http.createServer((req, res) => {
   const clearUrl = new URL(req.url, `http://${req.headers.host}`);
   const { pathname } = clearUrl;
 
-  const pathArr = pathname.split('/').filter((item) => item !== '');
-
-  if (pathArr[0] === 'file') {
-    const path = pathArr.splice(1).join('/') || 'index.html';
-
-    fs.readFile(`./public/${path}`, (err, data) => {
-      if (!err) {
-        res.statusCode = 200;
-        res.end(data);
-      }
-      
-      res.statusCode = 404;
-      res.end('404 Not Found');
-    });
-  } else {
+  if (!pathname.startsWith('/file')) {
     res.statusCode = 404;
     res.end('Please, use /file/');
   }
+
+  const pathArr = pathname
+    .split('/')
+    .filter((item) => item !== '' && item !== 'file');
+
+  const path = pathArr.splice(1).join('/') || 'index.html';
+
+  fs.readFile(`./public/${path}`, (err, data) => {
+    if (!err) {
+      res.statusCode = 200;
+      res.end(data);
+
+      /*
+      * if I don't return here,
+      * I receive an 'ERR_STREAM_WRITE_AFTER_END' error
+      * so I either have to return here or add an else statement
+      * like I did previously
+      ? if there is a better way to do this, please let me know
+      */
+
+      return;
+    }
+    res.statusCode = 404;
+    res.end('404 Not Found');
+  });
 });
 
 server.listen(PORT, () => {
