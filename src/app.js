735cc60a92b39c25b392be9e940e000aa81aa1e9
mr-valentos <nerpavlad@gmail.com>
2023-02-16 16:25:58 +0200

    Static files server

--- src/app.js
+++ src/app.js
@@ -1,21 +1,43 @@
 'use strict';
+/* eslint-disable no-console */
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
+const PORT = 3000;
 
-module.exports = sum;
+const server = http.createServer((req, res) => {
+  const normalizeURL = new URL(req.url, `http://${req.headers.host}`);
+
+  const parts = normalizeURL.pathname.slice(1).split('/');
+
+  if (parts[0] !== 'file') {
+    console.log('Write a correct address. Exp: /file/styles/main.css');
+  }
+
+  parts[0] = 'public';
+
+  const correctPath = parts.join('/');
+
+  (async() => {
+    try {
+      const fileData = await fs.readFile(
+        path.join(__dirname, '../', correctPath), 'utf-8');
+
+      res.setHeader('Content-Type', 'application/json');
+
+      res.end(JSON.stringify({
+        fileData,
+      }));
+    } catch (e) {
+      res.statusCode = 404;
+      res.statusMessage = 'Non existent file';
+      res.end();
+    }
+  })();
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server started! 🚀');
+});
