d68285b22d8e840c87a686e815b4c26c588a953e
Nazarii Kyselevych <nazarkysel@gmail.com>
2023-03-01 15:38:10 +0200

    v0.1

--- src/app.js
+++ src/app.js
@@ -1,21 +1,29 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = sum;
+const server = http.createServer((req, res) => {
+  if (!req.url.startsWith('/file')) {
+    res.end('pathname should start with `/file`');
+
+    return;
+  }
+
+  const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
+  const fileName = normalizedUrl.pathname
+    .replace(/\/file\/*/, '') || 'index.html';
+  const filePath = path.join(__dirname, 'public', fileName);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.statusCode = 404;
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000);
