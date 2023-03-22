1c2038924587dd48da70959758f788427da91447
Eugene Khablenko <eugene.khablenko@gmail.com>
2023-03-22 04:01:12 +0200

    added public folder

---
+++ src/request.js
@@ -0,0 +1,15 @@
+'use strict';
+const http = require('http');
+
+const options = {
+  hostname: 'localhost',
+  port: 3000,
+  path: '/file/styles/main.css',
+}
+
+http.get(options, (res) => {
+  res.setEncoding('utf8');
+  res.on('data', (data) => {
+    console.log(data);
+  })
+});
