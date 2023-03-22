41d2f213d062332e853cb0f1aaadd74b6ebc4ac9
Eugene Khablenko <eugene.khablenko@gmail.com>
2023-03-22 04:00:48 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,43 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const { log } = require('console');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const normalizedURL = new url.URL(req.url, `http://${req.headers.host}`);
+  const { pathname } = normalizedURL;
+
+  if (!pathname.startsWith('/file')) {
+    res.end('The path should start with /file/');
+    return;
+  };
+
+  const removeFile = pathname.replace('/file', '');
+  const returnFile = pathname.startsWith('/')
+    ? pathname.slice(1)
+    : pathname;
+
+  const filename = returnFile || 'index.html';
+
+  fs.readFile(`./public/${removeFile}`, (err, data) => {
+    if (err) {
+      res.statusCode = 404;
+      req.statusMessage = 'The requestis invalid';
+      res.end('The requested file does not exist');
+      return;
+    }
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`The server is running on http://localhost:${PORT}`);
+});
