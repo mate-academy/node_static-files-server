b3d73c71f8e904084a02efe8fbfadfef066a828a
Levente Krotos <krotos.levi@gmail.com>
2023-03-24 13:30:50 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,48 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((request, response) => {
+  const normalizedUrl = new URL(
+    request.url,
+    `http://${request.headers.host}`
+  );
+
+  const isFilePath = normalizedUrl.pathname
+    .slice(1)
+    .split('/')[0] === 'file';
+
+  const fileName = isFilePath
+    ? normalizedUrl.pathname
+      .split('/')
+      .slice(2)
+      .join('/')
+    : normalizedUrl.pathname
+      .slice(1);
+
+  fs.readFile(`./src/public/${fileName}`, (error, data) => {
+    if (!error && isFilePath) {
+      response.statusCode = 200;
+      response.end(data);
+
+      return;
+    } else if (!error && !isFilePath) {
+      response.statusCode = 404;
+      response.end('Maybe you dont used "/file/" before your requested file');
+
+      return;
+    }
+
+    response.statusCode = 404;
+
+    response.end('oops, file not found');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
