8dec7d8e147979e2e1b6961b0adc0df587f7ca91
Lilia Obushenko <liliaobushenko@gmail.com>
2023-04-14 20:12:32 +0300

    static files server was created

--- src/app.js
+++ src/app.js
@@ -1,21 +1,34 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = sum;
+const PORT = process.env.port || 3000;
+
+const server = http.createServer((req, res) => {
+  const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+  const filePath = normalizedURL.pathname;
+  const fileName = path.basename(filePath) || 'index.html';
+  const dirName = path.dirname(filePath);
+
+  if (dirName !== '/file') {
+    res.end('The pathname has to start with "/file/"');
+
+    return;
+  }
+
+  fs.readFile(`./src/public/${fileName}`, (err, data) => {
+    if (err) {
+      res.statusCode = 404;
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
