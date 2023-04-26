6a36eaf4839a645391fddf4846aefffc2e653e3b
Lilia Obushenko <liliaobushenko@gmail.com>
2023-04-26 17:41:18 +0300

    some fixes (subfolders)

--- src/app.js
+++ src/app.js
@@ -3,15 +3,13 @@
 
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
 
 const PORT = process.env.port || 3000;
 
 const server = http.createServer((req, res) => {
   const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
   const filePath = normalizedURL.pathname;
-  const fileName = path.basename(filePath);
-  const fileAbsolutePath = path.join(__dirname, 'public', fileName);
+  const fileName = filePath.slice(6) || 'index.html';
 
   if (!filePath.startsWith('/file')) {
     res.statusCode = 400;
@@ -20,13 +18,15 @@ const server = http.createServer((req, res) => {
     return;
   }
 
-  fs.readFile(fileAbsolutePath, (err, data) => {
+  fs.readFile(`../public/${fileName}`, (err, data) => {
     if (err) {
       res.statusCode = 404;
       res.end('File does not exist');
-    } else {
-      res.end(data);
+
+      return;
     }
+
+    res.end(data);
   });
 });
 
