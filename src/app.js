8910828ba7f11ec49096bb32b2303f7130877877
Lilia Obushenko <liliaobushenko@gmail.com>
2023-04-27 19:56:19 +0300

    absolute path fixed

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.port || 3000;
 
@@ -10,6 +11,7 @@ const server = http.createServer((req, res) => {
   const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
   const filePath = normalizedURL.pathname;
   const fileName = filePath.slice(6) || 'index.html';
+  const absolutePath = path.join(path.dirname(__dirname), 'public', fileName);
 
   if (!filePath.startsWith('/file')) {
     res.statusCode = 400;
@@ -18,7 +20,7 @@ const server = http.createServer((req, res) => {
     return;
   }
 
-  fs.readFile(`../public/${fileName}`, (err, data) => {
+  fs.readFile(absolutePath, (err, data) => {
     if (err) {
       res.statusCode = 404;
       res.end('File does not exist');
