a9c367d5da5754529f28f282381e2b64bd021ea1
Daria <hamdsad8@gmail.com>
2023-05-29 15:06:26 +0300

    created a static files server

--- src/app.js
+++ src/app.js
@@ -1,21 +1,38 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const normalizedUrl = new url.URL(req.url, `http://${req.headers.host}`);
+
+  if (!normalizedUrl.pathname.startsWith('/file/')) {
+    res.statusCode = 400;
+
+    res.end('The pathname to your file should start with "/file"');
+  }
+
+  const fileName = req.url.slice(6) || 'index.html';
+
+  fs.readFile(`./public/${fileName}`, 'utf8', (err, data) => {
+    if (err) {
+      res.statusCode = 400;
+
+      res.end('The pathname to your file does not exist');
+    }
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.statusCode = 200;
+
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
