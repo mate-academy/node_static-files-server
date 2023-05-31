0634b51710ab8d939eeafc18c1391a62f8d94261
Mark Boiko <boikomark555@gmail.com>
2023-05-31 16:36:47 +0300

    server static files

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
 
-module.exports = sum;
+http.createServer((req, res) => {
+  let fileName = req.url.slice(1);
+
+  if (fileName.slice(0, 4) !== 'file') {
+    res.statusCode = 404;
+    res.end('Your requst shoud contain /file/...');
+  }
+
+  fileName = fileName.slice(5) || 'index.html';
+
+  fs.readFile(`src/public/${fileName}`, 'utf8', (error, data) => {
+    if (!error) {
+      res.statusCode = 200;
+      res.end(data);
+    }
+
+    res.statusCode = 404;
+    res.end();
+  });
+});
