b4e2f809cfbf0508494edeaeae7aaa48a5d048db
Vadym Dubniak <dubnyakv@gmail.com>
2023-05-31 17:16:58 +0300

    created server that will return files

--- src/app.js
+++ src/app.js
@@ -1,21 +1,32 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
 
-module.exports = sum;
+const server = http.createServer((request, responce) => {
+  const normalizedUrl = new URL(request.url, `http://${request.headers.host}`);
+
+  if (!normalizedUrl.pathname.startsWith('/file')) {
+    responce.statusCode = 404;
+
+    responce.end('The pathname to your file should start with "/file"');
+  }
+
+  const fileName = request.url.slice(6) || 'index.html';
+
+  try {
+    const data = fs.readFileSync(`./public/${fileName}`, 'utf8');
+
+    responce.statusCode = 200;
+
+    responce.end(data);
+  } catch (error) {
+    responce.statusCode = 404;
+
+    responce.end(`${fileName} does not exist`);
+  }
+});
+
+module.exports = {
+  server,
+};
