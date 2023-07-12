ed0c538d9088af939b43d85e8362263f10e58de9
Yelisieiev Kim <rbv271@outlook.com>
2023-07-12 18:43:06 +0300

    fixes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
+const { getContentType } = require('./getContentType');
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,13 +18,20 @@ const server = http.createServer((req, res) => {
   }
 
   const pathToFile = pathParts.slice(1).join('/') || 'index.html';
+  const absolutePath = path.join(__dirname, '..', `public/${pathToFile}`);
 
-  fs.readFile(`./public/${pathToFile}`, (error, data) => {
+  fs.readFile(absolutePath, (error, data) => {
     if (error) {
       res.statusCode = 404;
       res.end('404 Error: File not found');
+
+      return;
     }
 
+    const ext = path.extname(absolutePath);
+    const contentType = getContentType(ext);
+
+    res.setHeader('Content-Type', contentType);
     res.end(data);
   });
 });
---
+++ src/getContentType.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function getContentType(ext) {
+  switch (ext) {
+    case '.html':
+      return 'text/html';
+    case '.css':
+      return 'text/css';
+    case '.js':
+      return 'text/javascript';
+    default:
+      return 'text/plain';
+  }
+};
+
+module.exports = { getContentType };
