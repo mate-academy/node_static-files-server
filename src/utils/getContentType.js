f3e8664182a831d0c6f042c4ad844f54e1ac95d5
ivanm2706 <ivanmorhun664@gmail.com>
2023-07-13 19:01:56 +0300

    solution

---
+++ src/utils/getContentType.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function getContentType(ext) {
+  switch (ext) {
+    case 'html':
+      return 'text/html';
+    case 'css':
+      return 'text/css';
+    case 'js':
+      return 'text/javascript';
+    default:
+      return 'text/plain';
+  }
+};
+
+module.exports = {
+  getContentType,
+};
