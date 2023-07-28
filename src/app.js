0df77b6d0a75736756a9416deb502e30f7c82903
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-07-28 12:08:35 +0300

    sol

--- src/app.js
+++ src/app.js
@@ -1,21 +1,49 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
-module.exports = sum;
+const server = http.createServer((req, res) => {
+  const normalizedUrl = new url.URL(req.url, `http://${req.headers.host}/`);
+  const urlParts = normalizedUrl.pathname.slice(1).split('/');
+
+  if (urlParts[0] !== 'file') {
+    res.statusCode = 400;
+    res.statusMessage = 'Wrong path';
+
+    res.end('Request path must start with /file');
+
+    return;
+  }
+
+  let filePath = `./src/public/${urlParts.slice(1).join('/')}`;
+
+  if (filePath === './src/public/') {
+    filePath = './src/public/index.html';
+  }
+
+  if (filePath.split('.')[filePath.split('.').length - 1] === 'html') {
+    res.setHeader('Content-Type', 'text/html');
+  } else {
+    res.setHeader('Content-Type', 'text/plain');
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.statusCode = 404;
+      res.statusMessage = 'File not found';
+
+      res.end('404 File not found');
+
+      return;
+    }
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    res.end(data);
+  });
+});
+
+server.listen(3000);
