bfd54bdcac36dfe951ee46121ccf677c43a31708
Anastasiia <svintsova.nastya@gmail.com>
2023-08-11 16:30:06 +0300

    feat: implement tsatic file server

--- src/app.js
+++ src/app.js
@@ -1,21 +1,43 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+const publicDir = './public';
+const filePrefix = '/file/';
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (!pathName.startsWith(filePrefix)) {
+    res.statusCode = 404;
+
+    res.end(
+      'To load files, use the /file/ prefix followed by the path to the file.'
+    );
+
+    return;
+  }
+
+  const fileName = pathName.substring(filePrefix.length);
+  const filePath = path.join(publicDir, fileName);
+
+  fs.readFile(filePath, (error, data) => {
+    if (!error) {
+      res.statusCode = 200;
+      res.end(data);
+
+      return;
+    }
+
+    res.statusCode = 404;
+    res.end('File wasn\'t found');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
