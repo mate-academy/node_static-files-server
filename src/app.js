6ea52f569c2c38f1227bb612e475c54e712ce6e9
artkas03 <zhonayer2017@gmail.com>
2023-08-15 18:14:15 +0300

    finished task

--- src/app.js
+++ src/app.js
@@ -1,21 +1,47 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+// eslint-disable-next-line max-len
+const WRONG_PATH_MESSAGE = 'No such file or path to file is invalid. Path shoud start with "/file" and relative path.';
+
+const server = http.createServer((req, res) => {
+  const filePath = req.url === '/file'
+    ? '/file/index.html'
+    : req.url;
+
+  res.headers = {
+    'Content-type': 'file',
+  };
+
+  try {
+    if (!filePath.startsWith('/file')) {
+      throw new Error(WRONG_PATH_MESSAGE);
+    }
+
+    const fullPath = path.join(__dirname, filePath)
+      .replace('src\\file', 'public');
+
+    if (!fs.existsSync(fullPath)) {
+      throw new Error(WRONG_PATH_MESSAGE);
+    }
+
+    const fullPathSecured = new url.URL(fullPath, `http://${req.headers.host}`);
+
+    const fileStream = fs.createReadStream(fullPathSecured.href);
+
+    fileStream.pipe(res);
+  } catch (err) {
+    res.statusCode = 404;
+
+    res.end(err.message);
+  }
+});
+
+server.listen(PORT, () => {
+  process.stdout.write(`Server is running on http://localhost:${PORT}\n`);
+});
