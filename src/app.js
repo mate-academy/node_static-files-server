7ec735e65edfd06f830728a8eb5788491754164d
Olena Boldysheva <adv55577@gmail.com>
2023-08-17 21:50:29 +0300

    add headers

--- src/app.js
+++ src/app.js
@@ -18,10 +18,30 @@ const server = http.createServer((req, res) => {
     const fileName
       = normalizedUrl.pathname.split('/').slice(2).join('/') || 'index.html';
 
+    const fileExtension = fileName.split('.').slice(-1);
+
+    const mimeType = {
+      html: 'text/html',
+      css: 'text/css',
+      js: 'text/javascript',
+      jpg: 'image/jpeg',
+      png: 'image/png',
+      json: 'application/json',
+      pdf: 'application/pdf',
+      mp4: 'video/mp4',
+      mp3: 'audio/mpeg',
+    };
+
+    if (!mimeType[fileExtension]) {
+      res.statusCode = 400;
+      res.end('File extension is not supported');
+    }
+
     fs.readFile(`./public/${fileName}`, (error, data) => {
       if (error) {
         res.statusCode = 404;
       } else {
+        res.setHeader('Content-Type', `${mimeType[fileExtension]}`);
         res.end(data);
       }
     });
