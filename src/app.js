afd18533e49e2f8eb8d979c39a3cf5bd2c00a016
artkas03 <zhonayer2017@gmail.com>
2023-08-18 14:09:03 +0300

    fixed bugs

--- src/app.js
+++ src/app.js
@@ -10,25 +10,31 @@ const PORT = process.env.PORT || 3000;
 const WRONG_PATH_MESSAGE = 'No such file or path to file is invalid. Path shoud start with "/file" and relative path.';
 
 const server = http.createServer((req, res) => {
-  const filePath = req.url === '/file'
-    ? '/file/index.html'
-    : req.url;
+  let filePath = req.url;
 
-  res.headers = {
-    'Content-type': 'file',
-  };
+  if (filePath.endsWith('/')) {
+    filePath = filePath.slice(0, -1);
+  }
+
+  if (filePath === '/file') {
+    filePath += '/index.html';
+  }
 
   try {
     if (!filePath.startsWith('/file')) {
       throw new Error(WRONG_PATH_MESSAGE);
     }
 
-    const fullPath = path.join(__dirname, filePath)
-      .replace('src\\file', 'public');
+    const fullPath = path
+      .join(__dirname, '..', filePath.replace('file', 'public'));
 
     if (!fs.existsSync(fullPath)) {
       throw new Error(WRONG_PATH_MESSAGE);
     }
+
+    res.headers = {
+      'Content-type': 'file',
+    };
 
     const fullPathSecured = new url.URL(fullPath, `http://${req.headers.host}`);
 
