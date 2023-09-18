2e7c48159c2355f08f00597c6a09fdeefcc33c79
Rodion Pushkarskiy <pushkarskiyrodion@gmail.com>
2023-09-18 19:20:18 +0300

    changes after code review

--- src/app.js
+++ src/app.js
@@ -2,20 +2,22 @@
 'use strict';
 
 const http = require('http');
+const path = require('path');
 const fs = require('fs');
 
 const PORT = 8080;
 
 const server = http.createServer((req, res) => {
-  const normalized = new URL(req.url, `http://${req.headers.host}`);
-  const path = normalized.pathname.slice(1);
-  const hasSymbolsAfterSlash = /\/[^/]+$/.test(path);
+  const { url, headers } = req;
+  const normalized = new URL(url, `http://${headers.host}`);
+  const pathname = normalized.pathname.slice(1);
+  const splitted = pathname.split('/');
+  const fileName = splitted[splitted.length - 1];
 
-  if (hasSymbolsAfterSlash) {
-    const splitted = path.split('/');
-    const fileName = splitted[splitted.length - 1];
+  if (fileName) {
+    const filePath = path.join('./public', fileName);
 
-    fs.readFile(`./public/${fileName}`, { encoding: 'utf8' }, (err, data) => {
+    fs.readFile(filePath, { encoding: 'utf8' }, (err, data) => {
       if (!err) {
         res.end(data);
 
