608ac54030a1bf0cebeb332799b7980799394ebd
Artem Kravets <kravetsartyom@gmail.com>
2023-09-19 12:29:17 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,52 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3001;
+
+const server = http.createServer((req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+
+  if (url.pathname.split('/')[1] !== 'file') {
+    res.statusCode = 404;
+    res.end('Path not found');
+  } else {
+    const filePath = path.join(__dirname, 'public',
+      url.pathname.split('/').slice(2).join('/'));
+
+    if (!filePath.startsWith(path.join(__dirname, 'public'))) {
+      res.statusCode = 403;
+      res.end('Forbidden');
+
+      return;
+    }
+
+    const fileExtension = path.extname(filePath).slice(1);
+
+    const mimeType = {
+      html: 'text/html',
+      css: 'text/css',
+      js: 'text/javascript',
+      jpg: 'image/jpeg',
+      png: 'image/png',
+      json: 'application/json',
+      pdf: 'application/pdf',
+    };
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end('File not found');
+      } else {
+        res.setHeader('Content-Type', mimeType[fileExtension] || 'text/plain');
+        res.end(data);
+      }
+    });
+  }
+});
+
+server.listen(PORT, () => {
+  // console.log removed
+});
