cc8332efc725d099335e159bc09d2586dc162dd4
Izzet Yusufov <17izzet@gmail.com12345Hello>
2023-10-11 22:06:02 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,47 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-type', 'application/json');
+
+  const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+
+  if (pathname !== '/favicon.ico') {
+    if (!pathname.startsWith('/file')) {
+      res.statusCode = 404;
+      res.statusMessage = 'Bad request';
+
+      res.end('Your path should start with /file/name_of_file');
+
+      return;
+    };
+
+    let fileName = pathname.slice(6);
+
+    if (pathname === '/file/' || pathname === '/file') {
+      fileName = 'index.html';
+    }
+
+    fs.readFile(`./public/${fileName}`, (err, data) => {
+      console.log(err);
+      console.log(data);
+
+      if (err) {
+        res.statusCode = 404;
+        res.statusMessage = 'File is not exist';
+        res.end();
+      }
+
+      res.end(data);
+    });
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`server running on: http://localHost:${PORT}`);
+});
