9549cbed748311f0b358b8a0d3df081d0da85c37
Sergey Mironenko <serg1prime@gmail.com>
2023-10-14 19:25:22 +0300

    Static files server first commit

--- src/app.js
+++ src/app.js
@@ -1,21 +1,43 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
+const PORT = process.env.PORT || 5700;
 
-module.exports = sum;
+const server = http.createServer((request, response) => {
+  const normalizedUrl = new URL(request.url, `http://${request.headers.host}`);
+  let path = normalizedUrl.pathname.split('/');
+
+  if (path[0] !== 'file') {
+    response.statusCode = 404;
+    response.end(JSON.stringify(
+      {
+        error: 'Filename mast start with "/file" directory',
+      }
+    ));
+  } else {
+    if (path.length === 1) {
+      path = 'index.html';
+    } else {
+      path = path.slice(1).join('/');
+    }
+
+    fs.readFile(`.public/${path}`, (err, data) => {
+      if (err) {
+        response.statusCode = 404;
+        response.end(JSON.stringify(
+          {
+            error: 'Error while reading a file',
+          }
+        ));
+      } else {
+        response.end(data);
+      }
+    });
+  }
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server started! 🚀');
+});
