21cdaf88c6a7bbf03a4a8fa285eebfa0778431d8
Anna Grechana <grechanaanna@gmial.com>
2023-12-06 23:06:06 +0200

    1st version

---
+++ src/request.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+'use strict';
+
+const http = require('http');
+
+const BASE = 'http://localhost:3007';
+
+const href = BASE + '/file/index.txt';
+const req = http.request(href, (res) => {
+  res.setEncoding('utf8');
+  console.log(res.statusCode);
+  res.on('data', console.log);
+});
+
+req.on('error', console.log('Erorr'));
+
+req.end();
