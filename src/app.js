170db9f2d4348a0468b97ff5fbb2cf58b8ba19c1
Alexandra Protyanova <alexandra.p@mate.academy>
2024-01-24 16:29:44 +0200

    added base test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+/* eslint-disable no-console */
 'use strict';
+
+const { createServer } = require('./createServer');
+
+createServer()
+  .listen(5701, () => {
+    console.log(`Server is running on http://localhost:${5701} 🚀`);
+    console.log('Available at http://localhost:5701');
+  });
