1c4d64664a403c58eb9a614df8d1db995fbf589e
jacekkosiba <jac.kosiba@gmail.com>
2024-01-30 11:47:25 +0100

    fix after review

--- src/createServer.js
+++ src/createServer.js
@@ -14,18 +14,8 @@ function createServer() {
       return;
     }
 
-    if (request.url.includes('..')) {
-      response.statusCode = 404;
-      response.end('/../ not allowed');
-      // I have no idea how to pass this test:
-      // 'should return 400 for traversal paths'
-      // maybe axios normalizes the path and prevents traversal paths?
-    }
-
     if (!request.url.startsWith('/file/')) {
       response.statusCode = 200;
-      // the tests demands that this status code is 200
-      // not sure if it's correct since this is not a successful response
       response.end('Pathname must start with `/file`');
 
       return;
