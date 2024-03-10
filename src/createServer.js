4030baec03ce4069e1c31ee34fb40a29d0aa4eb3
Alyona Nabok <alyonana42@gmail.com>
2024-03-10 16:40:18 +0200

    add fixes

--- src/createServer.js
+++ src/createServer.js
@@ -3,38 +3,45 @@
 
 const http = require("node:http");
 const fs = require("fs");
+const OK = 200;
+const BAD_REQUEST = 400;
+const NOT_FOUND = 404;
 
 function createServer() {
   return http.createServer((req, res) => {
-    if (req.url.includes("../")) {
-      res.writeHead(400, { "Content-Type": "text/plain" });
+    const url = req.url;
+
+    res.setHeader("Content-Type", "text/plain");
+
+    if (url.includes("../")) {
+      res.statusCode = BAD_REQUEST;
       res.end("Invalid path");
 
       return;
     }
 
-    if (!req.url.startsWith("/file/")) {
-      res.writeHead(200, { "Content-Type": "text/plain" });
+    if (!url.startsWith("/file/")) {
+      res.statusCode = OK;
       res.end("Hint: Routes must start with /file/");
 
       return;
     }
 
-    if (req.url.includes("//")) {
-      res.writeHead(404, { "Content-Type": "text/plain" });
+    if (url.includes("//")) {
+      res.statusCode = NOT_FOUND;
       res.end("Path has duplicated slashes");
 
       return;
     }
 
-    const fileName = req.url.replace("/file/", "") || "index.html";
+    const fileName = url.replace("/file/", "") || "index.html";
 
-    fs.readFile(`./public/${fileName}`, (err, data) => {
-      if (!err) {
-        res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile(`./public/${fileName}`, (error, data) => {
+      if (!error) {
+        res.writeHead(OK, { "Content-Type": "text/html" });
         res.end(data);
       } else {
-        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.statusCode = NOT_FOUND;
         res.end("Non-existent file");
       }
     });
