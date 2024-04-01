bff2fda0e474549cbb57ac64f24689c9d4f77514
Oleksandr <dedikad@gmail.com>
2024-04-01 20:38:16 +0200

    done

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,42 @@
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    if (pathname === '/file') {
+      return res.end('WARNING: must be "/file/fileName"');
+    }
+
+    if (!pathname.startsWith('/file/')) {
+      res.statusCode = 400;
+
+      return res.end('HINT: routes not starting with "/file/"');
+    }
+
+    if (pathname.includes('//')) {
+      res.statusCode = 404;
+
+      return res.end('ERROR: paths having duplicated slashes');
+    }
+
+    const path = pathname.replace('/file', '') || 'index.html';
+
+    fs.readFile(`./public${path}`, (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+
+        return res.end('ERROR: non-existent files');
+      }
+
+      res.end(data);
+    });
+  });
 }
 
 module.exports = {
