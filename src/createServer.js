f0bb2a5f3ba862da914962e0ff216a743a36ce00
HolyRaveOS <kitlaruk1995@gmail.com>
2024-04-06 17:07:16 +0300

    add solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,47 @@
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const urlPath = req.url;
+    const fileName = urlPath.replace('/file/', '') || 'index.html';
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    if (urlPath.includes('../')) {
+      res.statusCode = 400;
+      res.end("Don't use traversal paths");
+
+      return;
+    }
+
+    if (!urlPath.startsWith('/file/')) {
+      res.statusCode = 200;
+      res.end('Please prefix the file path with /file/ to load files.');
+
+      return;
+    }
+
+    if (urlPath.includes('//')) {
+      res.statusCode = 404;
+      res.end("Don't use duplicated slashes");
+
+      return;
+    }
+
+    fs.readFile(`./public/${fileName}`, (err, data) => {
+      if (!err) {
+        res.end(data);
+      } else {
+        res.statusCode = 404;
+        res.end('File not found');
+      }
+    });
+  });
+
+  return server;
 }
 
 module.exports = {
