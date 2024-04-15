3a9899e9ac75229d955605a43aea90d2b1d07f8f
Oleksandr Podvoiskyi <podvoyskiy01@gmail.com>
2024-04-15 18:11:21 +0300

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,50 @@
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+
+    const url = req.url;
+
+    if (!url.startsWith('/file/')) {
+      res.statusCode = 200;
+      res.end('You should provide the path, which starts with /file/');
+
+      return;
+    }
+
+    if (url.includes('../')) {
+      res.statusCode = 400;
+      res.end('No dots allowed in the path');
+
+      return;
+    }
+
+    if (url.includes('//')) {
+      res.statusCode = 404;
+      res.end('No doubleSlash allowed');
+
+      return;
+    }
+
+    const fileName = url.replace(/\/*file\/?/, '');
+
+    fs.readFile(`public/${fileName}`, (err, data) => {
+      if (!err) {
+        res.end(data);
+
+        return;
+      }
+
+      res.statusCode = 404;
+      res.end('Not Found');
+    });
+  });
+
+  return server;
 }
 
 module.exports = {
