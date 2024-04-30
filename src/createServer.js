efd2438b2d865040d71a90c1f30763e2495fee4c
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-04-30 11:04:59 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,66 @@
 'use strict';
 
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((request, response) => {
+    const hasDoubleSlash = /\/\//g;
+
+    if (hasDoubleSlash.test(request.url)) {
+      response.statusCode = 404;
+      response.statusMessage = 'Double slashes not supported.';
+      response.end('Hint: Message for routes not starting with /file/');
+
+      return;
+    }
+
+    const normalizedUrl = new URL(
+      request.url,
+      `http://${request.headers.host}`,
+    );
+    const pathName = normalizedUrl.pathname.slice(1);
+
+    if (fs.existsSync(path.resolve(__dirname, pathName))) {
+      response.statusCode = 400;
+      response.statusMessage = 'URL has traversal paths';
+      response.end();
+
+      return;
+    }
+
+    if (!normalizedUrl.pathname.startsWith('/file/')) {
+      response.setHeader('Content-Type', 'text/plain');
+      response.end('Hint: Pathname should start with "/file/" to load a file.');
+
+      return;
+    }
+
+    const pathNameNoFile =
+      pathName === 'file' || pathName === 'file/'
+        ? 'index.html'
+        : pathName.replace('file/', '');
+
+    const filePath = path.resolve(__dirname, `../public/${pathNameNoFile}`);
+
+    const checkFile = async () => {
+      try {
+        const file = await fs.promises.readFile(filePath);
+
+        response.write(file);
+        response.end();
+      } catch (error) {
+        response.setHeader('Content-Type', 'text/plain');
+        response.statusCode = 404;
+        response.end('File does not exist.');
+      }
+    };
+
+    checkFile();
+  });
+
+  return server;
 }
 
 module.exports = {
