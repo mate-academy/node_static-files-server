1542893642bdc3e860f54cfee3bbdbc41c394b06
Andrii Khomiak <an.khomiak@gmail.com>
2024-05-20 17:17:52 +0300

    task done

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,58 @@
 'use strict';
 
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.Server();
+
+  server.on('request', (req, res) => {
+    const nomalizedUrl = new url.URL(req.url, `http://${req.headers.host}`);
+
+    res.setHeader('Content-type', 'text/plain');
+
+    switch (true) {
+      case req.url.includes('//'):
+        res.statusCode = 404;
+        res.end();
+
+        break;
+
+      case nomalizedUrl.pathname === '/file':
+        fs.readFile(`./public/index.html`, (err, data) => {
+          if (err) {
+            res.statusCode = 404;
+            res.end();
+          }
+
+          res.end(data);
+        });
+
+        break;
+
+      case !nomalizedUrl.pathname.startsWith('/file/'):
+        res.statusCode = 400;
+        res.end('Path should begin with "/file"');
+
+        break;
+
+      default:
+        fs.readFile(
+          `./public/${nomalizedUrl.pathname.slice(6)}`,
+          (err, data) => {
+            if (err) {
+              res.statusCode = 404;
+              res.end('The file not exists');
+            }
+
+            res.end(data);
+          },
+        );
+    }
+  });
+
+  return server;
 }
 
 module.exports = {
