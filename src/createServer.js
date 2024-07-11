8e135130cbbc9b6a9638518e0f76595205ba3c26
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-07-11 14:59:22 +0300

    VB solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,40 @@
 'use strict';
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const { pathname } = new URL(`http://${req.headers.host}${req.url}`);
+
+    const requestedPath =
+      pathname.replace('/file', '').slice(1) || 'index.html';
+    const realPath = path.join(__dirname, '..', 'public', requestedPath);
+
+    res.setHeader('content-type', 'text/plain');
+
+    if (!pathname.startsWith('/file')) {
+      res.statusCode = 400;
+      res.end('access denied!');
+    }
+
+    if (pathname.includes('//')) {
+      res.statusCode = 404;
+      res.end('Do not use double slashes for the path');
+    }
+
+    fs.readFile(realPath, (err, data) => {
+      if (!err) {
+        res.statusCode = 200;
+        res.end(data);
+
+        return;
+      }
+
+      res.statusCode = 404;
+      res.end('File not found');
+    });
+  });
 }
 
 module.exports = {
