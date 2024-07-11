613ef3f9e50528eed73194fc411ebe61bc936fc9
Vlad Buldenko <98160877+vbuldenko@users.noreply.github.com>
2024-07-11 15:08:38 +0300

    VB solution async

--- src/createServer.js
+++ src/createServer.js
@@ -1,39 +1,38 @@
 'use strict';
+
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 function createServer() {
-  return http.createServer((req, res) => {
+  return http.createServer(async (req, res) => {
     const { pathname } = new URL(`http://${req.headers.host}${req.url}`);
-
     const requestedPath =
       pathname.replace('/file', '').slice(1) || 'index.html';
     const realPath = path.join(__dirname, '..', 'public', requestedPath);
 
-    res.setHeader('content-type', 'text/plain');
+    res.setHeader('Content-Type', 'text/plain');
 
     if (!pathname.startsWith('/file')) {
       res.statusCode = 400;
-      res.end('access denied!');
+      res.end('Access denied!');
+      return;
     }
 
     if (pathname.includes('//')) {
       res.statusCode = 404;
       res.end('Do not use double slashes for the path');
+      return;
     }
 
-    fs.readFile(realPath, (err, data) => {
-      if (!err) {
-        res.statusCode = 200;
-        res.end(data);
-
-        return;
-      }
-
+    try {
+      const data = await fs.readFile(realPath);
+      res.statusCode = 200;
+      res.end(data);
+    } catch (err) {
       res.statusCode = 404;
       res.end('File not found');
-    });
+    }
   });
 }
 
