7b930faf8b903771ab6e3998973d717e1e827557
Valentyn Sobol <valiksobol14@gmail.com>
2024-07-13 13:26:53 +0300

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
 'use strict';
 
-const { createServer } = require('./createServer');
+const { createServer, PORT } = require('./createServer');
 
-createServer().listen(5701, () => {
-  console.log(`Server is running on http://localhost:${5701} 🚀`);
-  console.log('Available at http://localhost:5701');
+createServer().listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT} 🚀`);
+  console.log(`Available at http://localhost:${PORT}`);
 });
--- src/createServer.js
+++ src/createServer.js
@@ -1,10 +1,47 @@
 'use strict';
 
+const http = require('http');
+const url = require('url');
+const PORT = 5701;
+const fs = require('fs');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const normalizedUrl = new url.URL(req.url, `http://${req.headers.host}`);
+    const fileName =
+      normalizedUrl.pathname.replace('/file', '').slice(1) || `index.html`;
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    if (normalizedUrl.pathname.includes('//')) {
+      res.statusCode = 404;
+      res.end('File not found');
+
+      return;
+    }
+
+    if (!normalizedUrl.pathname.startsWith('/file')) {
+      res.statusCode = 400;
+      res.end('Correct request is: "/file/<File_NAME>"');
+
+      return;
+    }
+
+    fs.readFile(`./public/${fileName}`, (err, data) => {
+      if (!err) {
+        res.statusCode = 200;
+        res.end(data);
+      } else {
+        res.statusCode = 404;
+        res.end('File not found');
+      }
+    });
+  });
+
+  return server;
 }
 
 module.exports = {
   createServer,
+  PORT,
 };
