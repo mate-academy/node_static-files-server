edb1c30a33db0dafe50b134aae29dd01d0ebdedc
deezhizyu <kalenskuja@gmail.com>
2024-08-11 13:46:18 +0200

    Fix all tests

--- src/createServer.js
+++ src/createServer.js
@@ -3,18 +3,12 @@
 const http = require('http');
 const fs = require('fs');
 
-const PREFIX = 'file';
-const PUBLIC_FOLDER = './public';
+const PREFIX = '/file';
+const PUBLIC_FOLDER = './public/';
 
 function createServer() {
   const server = http.createServer(async (req, res) => {
-    if (req.url.includes('..')) {
-      res.statusCode = 400;
-      res.end('Bad URL');
-
-      return;
-    }
-
+    // one test doesn't pass without this check
     if (req.url.includes('//')) {
       res.statusCode = 404;
       res.end('Bad path');
@@ -26,19 +20,15 @@ function createServer() {
 
     res.setHeader('Content-Type', 'text/plain');
 
-    const pathname = url.pathname.slice(1);
+    if (!url.pathname.startsWith(PREFIX)) {
+      res.statusCode = 400;
 
-    if (!pathname.startsWith(PREFIX)) {
-      res.statusCode = 200;
-
-      res.end(
-        `Path should start with '/file/'. For example: ${req.headers.host}/file/photo.jpg`,
-      );
+      res.end('Please, start your path with <file>');
 
       return;
     }
 
-    const filename = pathname.replace(PREFIX, '') || 'index.html';
+    const filename = url.pathname.replace(PREFIX, '').slice(1) || 'index.html';
     const filePath = `${PUBLIC_FOLDER}/${filename}`;
 
     if (!fs.existsSync(filePath)) {
@@ -48,15 +38,10 @@ function createServer() {
       return;
     }
 
-    try {
-      const file = await fs.promises.readFile(filePath, 'utf-8');
+    const file = await fs.readFileSync(filePath, 'utf-8');
 
-      res.statusCode = 200;
-      res.end(file);
-    } catch (error) {
-      res.statusCode = 404;
-      res.end('Not Found');
-    }
+    res.statusCode = 200;
+    res.end(file);
   });
 
   return server;
