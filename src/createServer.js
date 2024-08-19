f5fb154c56536e3206a6412d71e54a8edb15ed54
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-08-19 13:26:11 +0200

    add missing {}

--- src/createServer.js
+++ src/createServer.js
@@ -11,19 +11,19 @@ function createServer() {
     const file = path.replace('/file', '') || 'index.html';
 
     if (path.includes('//')) {
-      res.writeHead(404, 'Content-Type', 'text/plain');
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
 
       return res.end('Valid request');
     }
 
-    if (!path.startsWith('/file/')) {
-      res.writeHead(400, 'Content-Type', 'text/plain');
+    if (!path.startsWith('/file')) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
 
       return res.end('The request must start with /file/');
     }
 
     if (!fs.existsSync(`./public/${file}`)) {
-      res.writeHead(404, 'Content-Type', 'text/plain');
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
 
       return res.end('File does not exist');
     }
@@ -31,12 +31,12 @@ function createServer() {
     fs.readFile(`./public/${file}`, (err, data) => {
       if (!err) {
         res.statusCode = 200;
-        res.writeHead(200, 'Content-Type', 'text/plain');
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
 
         return res.end(data);
       }
 
-      res.writeHead(404, 'Content-Type', 'text/plain');
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end('Not foud');
     });
   });
