6066204b70e31a89860d8e3684629f2c1ef483f1
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-08-19 13:16:16 +0200

    refactor

--- src/createServer.js
+++ src/createServer.js
@@ -5,53 +5,41 @@ const fs = require('fs');
 const url = require('url');
 
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
-  const server = http.createServer((req, res) => {
+  return http.createServer((req, res) => {
     const normUrl = new url.URL(req.url, `http://${req.headers.host}`);
     const path = normUrl.pathname;
     const file = path.replace('/file', '') || 'index.html';
 
     if (path.includes('//')) {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Valid request');
+      res.writeHead(404, 'Content-Type', 'text/plain');
 
-      return;
+      return res.end('Valid request');
     }
 
     if (!path.startsWith('/file')) {
-      res.statusCode = 400;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('The request must start with /file/');
+      res.writeHead(400, 'Content-Type', 'text/plain');
 
-      return;
+      return res.end('The request must start with /file/');
     }
 
     if (!fs.existsSync(`./public/${file}`)) {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('File does not exist');
+      res.writeHead(404, 'Content-Type', 'text/plain');
 
-      return;
+      return res.end('File does not exist');
     }
 
     fs.readFile(`./public/${file}`, (err, data) => {
       if (!err) {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(data);
+        res.writeHead(200, 'Content-Type', 'text/plain');
 
-        return;
+        return res.end(data);
       }
 
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
+      res.writeHead(404, 'Content-Type', 'text/plain');
       res.end('Not foud');
     });
   });
-
-  return server;
 }
 
 module.exports = {
