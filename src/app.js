afbb140825a15cd5d57f52faa5a5415743c015df
Bodya Kutsyk <mkkbodya@gmail.com>
2024-09-23 21:29:01 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -7,3 +7,5 @@ createServer().listen(5701, () => {
   console.log(`Server is running on http://localhost:${5701} 🚀`);
   console.log('Available at http://localhost:5701');
 });
+
+
--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,50 @@
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const BASE = 'http://localhost:5701';
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    res.setHeader('Content-type', 'text/plain');
+    res.statusCode = 200;
+
+    if (req.url.includes('//')) {
+      res.statusCode = 404;
+      res.end('Duplicated shashes error!');
+
+      return;
+    }
+
+    const normalizedUrl = new URL(req.url, BASE);
+    const pathURL = normalizedUrl.pathname.slice(1);
+
+    if (req.url.includes('..')) {
+      res.statusCode = 400;
+      res.end('Don`t even try');
+
+      return;
+    }
+
+    if (req.url.slice(1, 6) !== 'file/') {
+      res.statusCode = 200;
+      res.end('Path should start with /file');
+
+      return;
+    }
+
+    const realPath = path.join(__dirname, '..', 'public', pathURL.slice(4));
+
+    try {
+      const file = fs.readFileSync(realPath, 'utf-8');
+
+      res.end(file);
+    } catch (error) {
+      res.statusCode = 404;
+      res.end('No such a file!');
+    }
+  });
 }
 
 module.exports = {
