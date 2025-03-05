a824c38a1b67a89781c656cd4bcc0ff1fc193a0d
Andrii Rykal <andreuka79@gmail.com>
2025-03-05 17:02:29 +0200

    1 test fall

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,53 @@
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const PUBLIC_DIR = path.join(__dirname, '../public');
+
+  return http.createServer((req, res) => {
+    const { url } = req;
+
+    if (!url.startsWith('/file/')) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      return res.end('Use /file/{fileName} to access files');
+    }
+
+    let requestedPath = decodeURIComponent(url.replace('/file/', ''));
+    requestedPath = requestedPath.replace(/\/{2,}/g, '/')
+    const normalizedPath = path.normalize(requestedPath);
+
+    if (normalizedPath.includes('..') || path.isAbsolute(normalizedPath)) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      return res.end('Bad Request');
+    }
+
+    const filePath = path.join(PUBLIC_DIR, normalizedPath);
+
+    if (!filePath.startsWith(PUBLIC_DIR)) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      return res.end('Bad Request');
+    }
+
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        return res.end('File not found');
+      }
+
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          return res.end('Internal Server Error');
+        }
+
+        res.writeHead(200);
+        res.end(data);
+      });
+    });
+  });
 }
 
 module.exports = {
