b8e4757994051722392f218fe1910aa7b16e7105
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2025-03-11 18:28:35 +0200

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,56 @@
 'use strict';
 
+const fs = require('node:fs');
+const path = require('node:path');
+const http = require('node:http');
+
+const PREFIX = '/file';
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+
+    if (pathname.includes('//')) {
+      res.writeHead(404, 'Not Found', { 'Content-type': 'text/plain' });
+
+      res.end(
+        `Please, use next format: http://${req.headers.host}${PREFIX}/<path_to_your_file>`,
+      );
+
+      return;
+    }
+
+    if (!pathname.startsWith(PREFIX)) {
+      res.writeHead(400, 'Bad request', { 'Content-type': 'text/plain' });
+
+      res.end(
+        `Please, use next format: http://${req.headers.host}${PREFIX}/<path_to_your_file>`,
+      );
+
+      return;
+    }
+
+    const fileName = path.join(
+      'public',
+      pathname === PREFIX || pathname === `${PREFIX}/`
+        ? 'index.html'
+        : pathname.replace(PREFIX, ''),
+    );
+
+    fs.readFile(fileName, (err, data) => {
+      if (err) {
+        res.writeHead(404, 'Not Found', { 'Content-type': 'text/plain' });
+        res.end(`404 Not Found`);
+
+        return;
+      }
+
+      res.writeHead(200, 'OK', { 'Content-type': 'text/plain' });
+      res.end(data);
+    });
+  });
+
+  return server;
 }
 
 module.exports = {
