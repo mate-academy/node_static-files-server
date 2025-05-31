04557c507cf5bb19b440aed2638977ec5981a0bd
Oleksandra Koshyk <sakoshik1984@gmail.com>
2025-05-31 22:43:34 +0200

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,52 @@
 'use strict';
 
+const http = require('http');
+const fsp = require('fs/promises');
+const path = require('path');
+
+const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer(async (req, res) => {
+    const normalizesUrl = new URL(req.url, `http://${req.headers.host}`);
+    const pathname = normalizesUrl.pathname;
+
+    res.setHeader('Content-Type', 'text/plain');
+
+    if (!pathname.startsWith('/file')) {
+      res.statusCode = 400;
+
+      return res.end('Invalid path. Use /file/<filename>');
+    }
+
+    const relativePath = pathname.slice(5) || '/index.html';
+    const normalizedPath = path
+      .normalize(relativePath)
+      .replace(/^(\.\.[/\\])+/, '');
+    const requestedFilePath = path.join(PUBLIC_DIR, normalizedPath);
+
+    if (!requestedFilePath.startsWith(PUBLIC_DIR)) {
+      res.writeHead(403, { 'Content-Type': 'text/plain' });
+
+      return res.end('Access denied');
+    }
+
+    try {
+      if (pathname.includes('//')) {
+        throw new Error();
+      }
+
+      const fileContent = await fsp.readFile(requestedFilePath);
+
+      res.statusCode = 200;
+      res.end(fileContent);
+    } catch (error) {
+      res.statusCode = 404;
+      res.end('Not found ERROR');
+    }
+  });
+
+  return server;
 }
 
 module.exports = {
