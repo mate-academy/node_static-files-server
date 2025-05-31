3d79993431391c17328c77b714f120a1f9e4974b
Oleksandra Koshyk <sakoshik1984@gmail.com>
2025-05-31 23:08:19 +0200

    fix bugs

--- src/createServer.js
+++ src/createServer.js
@@ -5,44 +5,45 @@ const fsp = require('fs/promises');
 const path = require('path');
 
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
+const START_PATH = '/file';
 
 function createServer() {
   const server = http.createServer(async (req, res) => {
-    const normalizesUrl = new URL(req.url, `http://${req.headers.host}`);
-    const pathname = normalizesUrl.pathname;
-
     res.setHeader('Content-Type', 'text/plain');
 
-    if (!pathname.startsWith('/file')) {
+    if (req.url.includes('//')) {
+      res.statusCode = 404;
+
+      return res.end('Not found');
+    }
+
+    const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
+    const pathname = normalizedUrl.pathname;
+
+    if (!pathname.startsWith(START_PATH)) {
       res.statusCode = 400;
 
-      return res.end('Invalid path. Use /file/<filename>');
+      return res.end(`Invalid path. Use $${START_PATH}/<filename>`);
     }
 
-    const relativePath = pathname.slice(5) || '/index.html';
-    const normalizedPath = path
-      .normalize(relativePath)
-      .replace(/^(\.\.[/\\])+/, '');
-    const requestedFilePath = path.join(PUBLIC_DIR, normalizedPath);
-
-    if (!requestedFilePath.startsWith(PUBLIC_DIR)) {
-      res.writeHead(403, { 'Content-Type': 'text/plain' });
-
-      return res.end('Access denied');
-    }
+    const relativePath = pathname.slice(START_PATH.length) || '/index.html';
 
     try {
-      if (pathname.includes('//')) {
-        throw new Error();
+      const requestedPath = path.resolve(PUBLIC_DIR, '.' + relativePath);
+
+      if (!requestedPath.startsWith(PUBLIC_DIR)) {
+        res.statusCode = 404;
+
+        return res.end('Access denied');
       }
 
-      const fileContent = await fsp.readFile(requestedFilePath);
+      const fileContent = await fsp.readFile(requestedPath);
 
       res.statusCode = 200;
       res.end(fileContent);
     } catch (error) {
       res.statusCode = 404;
-      res.end('Not found ERROR');
+      res.end('Not found');
     }
   });
 
