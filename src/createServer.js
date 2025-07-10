ab636b1b62627078670dad419c34105dbd7753b0
Stas Borychevskyi <sborichevskiy74@gmail.com>
2025-07-10 18:34:33 +0300

    Solution 1.0

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,62 @@
 'use strict';
 
+const fsp = require('fs/promises');
+const http = require('http');
+const url = require('url');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer(async (req, res) => {
+    const normalizedURL = new url.URL(req.url, `http://${req.headers.host}`);
+    const normalizedPath =
+      normalizedURL.pathname.replace(/^\/file\//, '') || 'index.html';
+    // Check for invalid paths first
+
+    if (!normalizedURL.pathname.startsWith('/file')) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Invalid file path');
+    }
+    // Check for path traversal attempts
+
+    if (normalizedURL.pathname.includes('..')) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Invalid file path');
+    }
+    // Check for double slashes
+
+    if (normalizedPath.includes('//')) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Paths have duplicated slashes');
+    }
+    // Show hint for /file endpoint
+
+    if (!normalizedURL.pathname.startsWith('/file/')) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Path should start with "/file/"');
+    }
+
+    try {
+      const file = await fsp.readFile(`./public/${normalizedPath}`, 'utf-8');
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(file);
+    } catch (error) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Not Found');
+    }
+  });
+
+  return server;
 }
 
 module.exports = {
