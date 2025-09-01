ec85f034bef2035911fd4a5b60aa49c137d2fec1
Nadiia Bulmak <nadgaval@gmail.com>
2025-09-01 16:42:27 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,10 +1,77 @@
 'use strict';
 
+const path = require('node:path');
+const http = require('node:http');
+const fsp = require('node:fs/promises');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer(async (req, res) => {
+    const url = new URL(req.url || '', `http://${req.headers.host}`);
+    const pathname = url.pathname;
+
+    if (!pathname.startsWith('/file')) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Use /file/<filename> to load files');
+    }
+
+    const requestedPath =
+      pathname === '/file' || pathname === '/file/'
+        ? 'index.html'
+        : pathname.replace(/^\/file\/?/, '');
+
+    if (/\/{2,}/.test(requestedPath)) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Not Found');
+    }
+
+    const publicDir = path.resolve(__dirname, '..', 'public');
+    const normalizedPath = path.normalize(requestedPath);
+    const realPath = path.resolve(publicDir, normalizedPath);
+
+    const relative = path.relative(publicDir, realPath);
+
+    if (relative.split(path.sep)[0] === '..') {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Bad Request');
+    }
+
+    try {
+      const file = await fsp.readFile(realPath);
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', getContentType(realPath));
+      res.end(file);
+    } catch {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Not Found');
+    }
+  });
+
+  return server;
 }
 
-module.exports = {
-  createServer,
-};
+function getContentType(filePath) {
+  const ext = path.extname(filePath).toLowerCase();
+
+  switch (ext) {
+    case '.css':
+      return 'text/css';
+    case '.js':
+      return 'application/javascript';
+    case '.json':
+      return 'application/json';
+    case '.html':
+      return 'text/plain';
+    default:
+      return 'text/plain';
+  }
+}
+
+module.exports = { createServer };
