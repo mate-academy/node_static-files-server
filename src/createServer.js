07d98c4e70b55e5dd30013b219cb84dc4d043be7
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-10-03 23:04:34 +0300

    fix2

--- src/createServer.js
+++ src/createServer.js
@@ -4,29 +4,7 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
-// const MIME_TYPES = {
-//   '.html': 'text/html; charset=utf-8',
-//   '.js': 'application/javascript; charset=utf-8',
-//   '.css': 'text/css; charset=utf-8',
-//   '.json': 'application/json; charset=utf-8',
-//   '.png': 'image/png',
-//   '.jpg': 'image/jpeg',
-//   '.jpeg': 'image/jpeg',
-//   '.gif': 'image/gif',
-//   '.svg': 'image/svg+xml; charset=utf-8',
-//   '.txt': 'text/plain; charset=utf-8',
-// };
-
-// function getMimeType(filePath) {
-//   return (
-//     MIME_TYPES[path.extname(filePath).toLowerCase()] ||
-//     'application/octet-stream'
-//   );
-// }
-
 function createServer() {
-  // const publicDir = path.resolve(__dirname, '../public');
-
   const server = http.createServer((req, res) => {
     const { pathname } = new URL(`http://${req.headers.host}${req.url}`);
     const requestedPath =
