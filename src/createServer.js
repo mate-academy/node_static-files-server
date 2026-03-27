11d0859f89f3aa72345c09c982e169f45b6270f8
adonch <donch.a080@gmail.com>
2026-03-27 21:28:11 +0200

    feat: implement HTTP server with file handling and error responses
    - Create a basic HTTP server that serves files from a public directory.
    - Handle invalid paths and provide appropriate HTTP status codes.
    - Ensure requests to /file/ prefix are processed correctly.

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,46 @@
 'use strict';
 
+const http = require('http');
+const fs = require('fs');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    let pathname = url.pathname;
+
+    if (req.url.includes('..')) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Invalid path');
+
+      return;
+    }
+
+    if (pathname.includes('//')) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+
+      return;
+    }
+
+    if (pathname === '/file' || pathname === '/file/') {
+      pathname = '/file/index.html';
+    }
+
+    if (pathname.startsWith('/file/')) {
+      fs.readFile(`.${pathname.replace('file', 'public')}`, (err, data) => {
+        if (err) {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end('File Not Found');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end(data);
+        }
+      });
+    } else {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.end('Please use /file/ prefix to load files');
+    }
+  });
 }
 
 module.exports = {
