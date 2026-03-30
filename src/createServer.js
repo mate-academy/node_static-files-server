75298fd779b58a189d2c6347f9347243f2c19a6c
Yaroslav <yaroslavhohulia@gmail.com>
2026-03-30 17:51:20 +0300

    added solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,10 +1,57 @@
+/* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+const path = require('path');
+const fsp = require('fs/promises');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer(async (req, res) => {
+    if (req.url.includes('//')) {
+      res.statusCode = 404;
+      res.end('Not Found');
+
+      return;
+    }
+
+    const pathname = req.url.slice(1);
+
+    const [action, ...rest] = pathname.split('/');
+    const fileName = rest.join('/');
+
+    if (action !== 'file') {
+      res.statusCode = 400;
+
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('To load file, you need use "/file/fileName"');
+
+      return;
+    }
+
+    if (!fileName) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Enter the correct path');
+
+      return;
+    }
+
+    const realPath = path.join(__dirname, '..', 'public', fileName);
+
+    try {
+      const file = await fsp.readFile(realPath, 'utf-8');
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(file);
+    } catch (error) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Not Found');
+    }
+  });
+
+  return server;
 }
 
-module.exports = {
-  createServer,
-};
+module.exports = { createServer };
