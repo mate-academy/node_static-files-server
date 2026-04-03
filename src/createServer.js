6398ce79d08f16091cb5daae6a093446e2dfba7c
Yan Kazymyr <kazymyr.yan@gmail.com>
2026-04-03 22:59:12 +0300

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,48 @@
 'use strict';
 
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+
+    if (url.pathname === '/file') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Please specify a file path after /file/.');
+
+      return;
+    }
+
+    if (!url.pathname.startsWith('/file')) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.end('Use /file/<path> to load files');
+
+      return;
+    }
+
+    const relativePath = url.pathname.replace(/^\/file\/?/, '') || 'index.html';
+    const publicDir = path.resolve(__dirname, '..', 'public');
+    const fullPath = path.resolve(publicDir, relativePath);
+
+    if (!fullPath.startsWith(publicDir + path.sep) && fullPath !== publicDir) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+
+      return;
+    }
+
+    try {
+      const data = fs.readFileSync(fullPath);
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    } catch (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+    }
+  });
 }
 
 module.exports = {
