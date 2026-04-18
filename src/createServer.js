24deacbc2a27b842ccb68a0a2d03158882063363
Dmytro Halieba <dmytro.halyeba@gmail.com>
2026-04-18 17:47:52 +0300

    feat: server for returning files using path

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,75 @@
 'use strict';
 
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+
+const CONTENT_TYPES = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  errors: 'text/plain',
+  hint: 'text/plain',
+};
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const normalizedUrl = new URL(req.url, 'http://localhost:5700');
+    const pathname = normalizedUrl.pathname;
+
+    if (pathname === '/file') {
+      res.writeHead(200, 'OK', {
+        'Content-Type': CONTENT_TYPES.hint,
+      });
+      res.end('Use /file/<filename> to load files');
+
+      return;
+    }
+
+    if (pathname.includes('../')) {
+      res.writeHead(400, 'Bad Request', {
+        'Content-Type': CONTENT_TYPES.errors,
+      });
+      res.end('Attempt to access files outside public folder');
+
+      return;
+    }
+
+    if (pathname.includes('//')) {
+      res.writeHead(404, 'Bad Request', {
+        'Content-Type': CONTENT_TYPES.errors,
+      });
+      res.end('Incorect path');
+
+      return;
+    }
+
+    if (!pathname.startsWith('/file/')) {
+      res.writeHead(200, 'OK', {
+        'Content-Type': CONTENT_TYPES.hint,
+      });
+      res.end('Use /file/<filename> to load files');
+
+      return;
+    }
+
+    const filePath = path.join('public', pathname.slice('/file/'.length));
+
+    fs.readFile(filePath, (err, data) => {
+      const existent = path.extname(filePath);
+
+      if (err) {
+        res.writeHead(404, 'Not Found', { 'Content-Type': 'text/plain' });
+        res.end('File not found');
+
+        return;
+      }
+
+      res.writeHead(200, 'OK', {
+        'Content-Type': CONTENT_TYPES[existent],
+      });
+      res.end(data);
+    });
+  });
 }
 
 module.exports = {
